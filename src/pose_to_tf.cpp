31d718cb6f162d9223d06e73f49caa38055bd804
mkam <mkam2@jh.edu>
2022-12-29 17:05:32 -0500

    added launch file + modified cpp

--- src/pose_to_tf.cpp
+++ src/pose_to_tf.cpp
@@ -3,6 +3,7 @@
 #include <geometry_msgs/PoseStamped.h>
 
 std::string child_frame;
+std::string base_frame;
 
 void poseStampedCallback(const geometry_msgs::PoseStampedConstPtr& msg){
   static tf::TransformBroadcaster br;
@@ -15,13 +16,14 @@ void poseStampedCallback(const geometry_msgs::PoseStampedConstPtr& msg){
                    msg->pose.orientation.z, 
                    msg->pose.orientation.w);
   transform.setRotation(q);
-  br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "base_frame", child_frame));
+  br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), base_frame, child_frame));
 }
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "pose_to_tf");
   ros::NodeHandle node;
-  ros::param::get("child_frame", child_frame);
+  ros::param::get("~base_frame" , base_frame);
+  ros::param::get("~child_frame", child_frame);
   ros::Subscriber sub = node.subscribe("/pose_topic", 11, &poseStampedCallback);
 
   ros::spin();
